24c4227847c9009519da56e96373fb97e4007ba3
emegua <emeguag@gmail.com>
2024-09-30 14:57:39 -0700

    init

---
+++ utils/dnp3_src/CMakeFiles/asiodnp3.dir/DependInfo.cmake
@@ -0,0 +1,53 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/cpp/libs/src/asiodnp3/ConsoleLogger.cpp" "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/CMakeFiles/asiodnp3.dir/cpp/libs/src/asiodnp3/ConsoleLogger.cpp.o"
+  "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/cpp/libs/src/asiodnp3/Conversions.cpp" "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/CMakeFiles/asiodnp3.dir/cpp/libs/src/asiodnp3/Conversions.cpp.o"
+  "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/cpp/libs/src/asiodnp3/DNP3Channel.cpp" "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/CMakeFiles/asiodnp3.dir/cpp/libs/src/asiodnp3/DNP3Channel.cpp.o"
+  "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/cpp/libs/src/asiodnp3/DNP3Manager.cpp" "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/CMakeFiles/asiodnp3.dir/cpp/libs/src/asiodnp3/DNP3Manager.cpp.o"
+  "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/cpp/libs/src/asiodnp3/DNP3ManagerImpl.cpp" "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/CMakeFiles/asiodnp3.dir/cpp/libs/src/asiodnp3/DNP3ManagerImpl.cpp.o"
+  "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/cpp/libs/src/asiodnp3/DefaultListenCallbacks.cpp" "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/CMakeFiles/asiodnp3.dir/cpp/libs/src/asiodnp3/DefaultListenCallbacks.cpp.o"
+  "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/cpp/libs/src/asiodnp3/DefaultMasterApplication.cpp" "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/CMakeFiles/asiodnp3.dir/cpp/libs/src/asiodnp3/DefaultMasterApplication.cpp.o"
+  "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/cpp/libs/src/asiodnp3/ErrorCodes.cpp" "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/CMakeFiles/asiodnp3.dir/cpp/libs/src/asiodnp3/ErrorCodes.cpp.o"
+  "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/cpp/libs/src/asiodnp3/IOHandler.cpp" "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/CMakeFiles/asiodnp3.dir/cpp/libs/src/asiodnp3/IOHandler.cpp.o"
+  "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/cpp/libs/src/asiodnp3/LinkSession.cpp" "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/CMakeFiles/asiodnp3.dir/cpp/libs/src/asiodnp3/LinkSession.cpp.o"
+  "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/cpp/libs/src/asiodnp3/MasterScan.cpp" "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/CMakeFiles/asiodnp3.dir/cpp/libs/src/asiodnp3/MasterScan.cpp.o"
+  "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/cpp/libs/src/asiodnp3/MasterSessionStack.cpp" "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/CMakeFiles/asiodnp3.dir/cpp/libs/src/asiodnp3/MasterSessionStack.cpp.o"
+  "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/cpp/libs/src/asiodnp3/MasterStack.cpp" "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/CMakeFiles/asiodnp3.dir/cpp/libs/src/asiodnp3/MasterStack.cpp.o"
+  "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/cpp/libs/src/asiodnp3/MasterTCPServer.cpp" "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/CMakeFiles/asiodnp3.dir/cpp/libs/src/asiodnp3/MasterTCPServer.cpp.o"
+  "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/cpp/libs/src/asiodnp3/OutstationStack.cpp" "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/CMakeFiles/asiodnp3.dir/cpp/libs/src/asiodnp3/OutstationStack.cpp.o"
+  "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/cpp/libs/src/asiodnp3/PrintingCommandCallback.cpp" "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/CMakeFiles/asiodnp3.dir/cpp/libs/src/asiodnp3/PrintingCommandCallback.cpp.o"
+  "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/cpp/libs/src/asiodnp3/PrintingSOEHandler.cpp" "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/CMakeFiles/asiodnp3.dir/cpp/libs/src/asiodnp3/PrintingSOEHandler.cpp.o"
+  "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/cpp/libs/src/asiodnp3/SerialIOHandler.cpp" "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/CMakeFiles/asiodnp3.dir/cpp/libs/src/asiodnp3/SerialIOHandler.cpp.o"
+  "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/cpp/libs/src/asiodnp3/TCPClientIOHandler.cpp" "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/CMakeFiles/asiodnp3.dir/cpp/libs/src/asiodnp3/TCPClientIOHandler.cpp.o"
+  "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/cpp/libs/src/asiodnp3/TCPServerIOHandler.cpp" "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/CMakeFiles/asiodnp3.dir/cpp/libs/src/asiodnp3/TCPServerIOHandler.cpp.o"
+  "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/cpp/libs/src/asiodnp3/UpdateBuilder.cpp" "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/CMakeFiles/asiodnp3.dir/cpp/libs/src/asiodnp3/UpdateBuilder.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ASIO_STANDALONE"
+  "asiodnp3_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "deps/asio/asio/include"
+  "./cpp/libs/src"
+  "./cpp/libs/include"
+  "./cpp/tests/libs/src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/CMakeFiles/asiopal.dir/DependInfo.cmake"
+  "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/CMakeFiles/opendnp3.dir/DependInfo.cmake"
+  "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/CMakeFiles/openpal.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ utils/dnp3_src/CMakeFiles/openpal.dir/DependInfo.cmake
@@ -0,0 +1,46 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/cpp/libs/src/openpal/container/Buffer.cpp" "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/CMakeFiles/openpal.dir/cpp/libs/src/openpal/container/Buffer.cpp.o"
+  "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/cpp/libs/src/openpal/container/RSlice.cpp" "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/CMakeFiles/openpal.dir/cpp/libs/src/openpal/container/RSlice.cpp.o"
+  "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/cpp/libs/src/openpal/container/WSlice.cpp" "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/CMakeFiles/openpal.dir/cpp/libs/src/openpal/container/WSlice.cpp.o"
+  "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/cpp/libs/src/openpal/executor/MonotonicTimestamp.cpp" "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/CMakeFiles/openpal.dir/cpp/libs/src/openpal/executor/MonotonicTimestamp.cpp.o"
+  "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/cpp/libs/src/openpal/executor/TimeDuration.cpp" "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/CMakeFiles/openpal.dir/cpp/libs/src/openpal/executor/TimeDuration.cpp.o"
+  "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/cpp/libs/src/openpal/executor/TimerRef.cpp" "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/CMakeFiles/openpal.dir/cpp/libs/src/openpal/executor/TimerRef.cpp.o"
+  "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/cpp/libs/src/openpal/logging/Logger.cpp" "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/CMakeFiles/openpal.dir/cpp/libs/src/openpal/logging/Logger.cpp.o"
+  "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/cpp/libs/src/openpal/logging/StringFormatting.cpp" "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/CMakeFiles/openpal.dir/cpp/libs/src/openpal/logging/StringFormatting.cpp.o"
+  "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/cpp/libs/src/openpal/serialization/ByteSerialization.cpp" "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/CMakeFiles/openpal.dir/cpp/libs/src/openpal/serialization/ByteSerialization.cpp.o"
+  "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/cpp/libs/src/openpal/serialization/DoubleFloat.cpp" "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/CMakeFiles/openpal.dir/cpp/libs/src/openpal/serialization/DoubleFloat.cpp.o"
+  "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/cpp/libs/src/openpal/serialization/FloatByteOrder.cpp" "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/CMakeFiles/openpal.dir/cpp/libs/src/openpal/serialization/FloatByteOrder.cpp.o"
+  "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/cpp/libs/src/openpal/serialization/Format.cpp" "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/CMakeFiles/openpal.dir/cpp/libs/src/openpal/serialization/Format.cpp.o"
+  "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/cpp/libs/src/openpal/serialization/Parse.cpp" "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/CMakeFiles/openpal.dir/cpp/libs/src/openpal/serialization/Parse.cpp.o"
+  "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/cpp/libs/src/openpal/serialization/SingleFloat.cpp" "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/CMakeFiles/openpal.dir/cpp/libs/src/openpal/serialization/SingleFloat.cpp.o"
+  "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/cpp/libs/src/openpal/serialization/UInt48LE.cpp" "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/CMakeFiles/openpal.dir/cpp/libs/src/openpal/serialization/UInt48LE.cpp.o"
+  "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/cpp/libs/src/openpal/util/Limits.cpp" "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/CMakeFiles/openpal.dir/cpp/libs/src/openpal/util/Limits.cpp.o"
+  "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/cpp/libs/src/openpal/util/ToHex.cpp" "/media/aicps/home/yonatan/OpenPLC_v3/utils/dnp3_src/CMakeFiles/openpal.dir/cpp/libs/src/openpal/util/ToHex.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ASIO_STANDALONE"
+  "openpal_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "deps/asio/asio/include"
+  "./cpp/libs/src"
+  "./cpp/libs/include"
+  "./cpp/tests/libs/src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
